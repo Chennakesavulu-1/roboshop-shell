bea2e7a951b2563b308d5006f8963a8f30d30da0
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-23 20:36:12 +0530

    roboshop scripting

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,8 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-vim /etc/nginx/default.d/roboshop.conf
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 
 #we need to copy config file here
