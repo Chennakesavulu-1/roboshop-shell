b441ee336a51df8efd7e905738a1d24cd43204c1
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-25 08:33:49 +0530

    roboshop scripting, mongodb installing

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,8 @@ echo -e "\e[33m Copy Mongodb Repo File\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[33m installing Mongodb server\e[0m"
 yum install mongodb-org -y
+echo -e "\e[33mUpdate mongoDB listen Adress\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 
 #modefiy the config file
 echo -e "\e[33m Start Mongodb service\e[0m"
