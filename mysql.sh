224445bc0c9689ee267b71a6687f6ff6d43ef2ae
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-25 17:42:28 +0530

    roboshop scripting, mysql  service installing

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,8 @@
 echo -e "\e[33m Disabel mysql version \e[0m"
 yum module disable mysql -y &>>/tmp/roboshop.log
+echo -e "\e[33m Copy mysql repo file\e[0m"
+cp /home/centos/roboshop-shell/mysql.repos /etc/yum.repos.d/mysql.repo
+
 echo -e "\e[33m Install mysql community server\e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 echo -e "\e[33m  start mysql service\e[0m"
