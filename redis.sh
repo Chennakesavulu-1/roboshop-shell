8ffe5d927fb6f20e98ca014c26d7080e52be331d
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-25 14:58:51 +0530

    roboshop scripting, Redish service installing

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[33m Install Redis\e[0m"
 yum install redis -y &>>/tmp/roboshop.log
 echo -e "\e[33m Update Redis listen redis address\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 echo -e "\e[33m Start redis service \e[0m"
 systemctl enable redis &>>/tmp/roboshop.log
 systemctl restart redis &>>/tmp/roboshop.log
