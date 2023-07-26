5c38d5bf1a475e21b2a035e7b94ebd0f1152b2f2
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-26 18:43:26 +0530

    updated payment scripting newely added set command

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,26 @@
-echo -e "\e[33m Installing nginx Server\e[0m"
-yum install nginx -y
 
 
+echo -e "${color} Installing nginx Server${nocolor}"
+yum install nginx -y &>>$log_file
 
-echo -e "\e[33m Removing old App content\e[0m"
-rm -rf /usr/share/nginx/html/*
-echo -e "\e[33m Download Frontend Contend\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[33m Extrated Frontend Content\e[0m"
+echo -e "${color} Removing old App content${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-echo -e "\e[33m Update Frontend Configuration\e[0m"
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "${color} Download Frontend Contend${nocolor}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+
+
+echo -e "${color} Extrated Frontend Content${nocolor}"
+
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+
+
+echo -e "${color} Update Frontend Configuration${nocolor}"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
 
 #we need to copy config file here
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 
 
