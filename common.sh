e706575d510e2dd3913b43a6fca0969b1b238edd
chennakesava <chennakesavulukallagunta@gmail.com>
2023-07-26 18:29:18 +0530

    updated payment scripting

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 color="\e[35m"
 nocolor="${nocolor}"
-log_file="/tmp/roboshop.log"
+log_file="$log_file"
 app_path="/app"
 
 app_presetup() {
@@ -8,8 +8,8 @@ app_presetup() {
   useradd roboshop &>>$log_file
 
   echo -e "${color} Create application directory${nocolor}"
-  rm -rf /app &>>/tmp/roboshop.log &>>$log_file
-  mkdir /app &>>/tmp/roboshop.log &>>$log_file
+  rm -rf /app &>>$log_file &>>$log_file
+  mkdir /app &>>$log_file &>>$log_file
 
   echo -e "${color} Download Application Content${nocolor}"
   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
@@ -59,62 +59,44 @@ mongo_schema_setup() {
 
 mysql_schema_setup(){
   echo -e "${color} Install mysql client${nocolor}"
-  yum install mysql -y &>>/tmp/roboshop.log &>>$log_file
+  yum install mysql -y &>>$log_file &>>$log_file
 
   echo -e "${color} Load schema${nocolor}"
-  mysql -h mysql-dev.chennadevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>/tmp/roboshop.log &>>$log_file
+  mysql -h mysql-dev.chennadevops.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>$log_file &>>$log_file
 
 }
 
 maven() {
   echo -e "${color} Install MAVEN ${nocolor}"
-  yum install maven -y &>>/tmp/roboshop.log &>>$log_file
+  yum install maven -y &>>$log_file &>>$log_file
 
   app_presetup
 
-
-
   echo -e "${color} Download application content ${nocolor}"
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>/tmp/roboshop.log &>>$log_file
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file &>>$log_file
   cd ${app_path}
-
-
 
   echo -e "${color} Download maven dependencies${nocolor}"
   mvn clean package &>>$log_file
 
 
-  mv target/${component}-1.0.jar ${component}.jar &>>/tmp/roboshop.log &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file &>>$log_file
 
   mysql_schema_setup
-
-
 
   systemd_setup
 }
 
 python() {
   echo -e "${color} Install Python${nocolor}"
-  yum install python36 gcc python3-devel -y &>>/tmp/roboshop.log
-  echo -e "${color} Add user application${nocolor}"
-  useradd roboshop &>>/tmp/roboshop.log
-  echo -e "${color} make directory ${nocolor}"
-  mkdir /app &>>/tmp/roboshop.log
-  echo -e "${color} download application content${nocolor}"
-  curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
-  cd /app
-  echo -e "${color} Extract zip file of payment${nocolor}"
-  unzip /tmp/payment.zip &>>/tmp/roboshop.log
+  yum install python36 gcc python3-devel -y &>>$log_file
+
+  app_presetup
 
   echo -e "${color} Install application dependencies${nocolor}"
   cd /app
-  pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
+  pip3.6 install -r requirements.txt &>>$log_file
 
-  echo -e "${color} Start SystemD services${nocolor}"
-  cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
-  echo -e "${color} Start payment service${nocolor}"
-  systemctl daemon-reload &>>/tmp/roboshop.log
-  systemctl enable payment &>>/tmp/roboshop.log
-  systemctl restart payment &>>/tmp/roboshop.log
+  systemd_setup
 
 }
